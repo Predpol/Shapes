018424086f700b30c38521460f1b2347c995aa87
Omar Qazi <omar@predpol.com>
2012-12-29 17:41:44 -0800

    Implemented open function

--- ext/shapes/shapes.c
+++ ext/shapes/shapes.c
@@ -3,12 +3,24 @@
 #include <stdlib.h>
 #include "shapefil.h"
 #include "shapes.h"
+
+VALUE shapeClass;
+	
 void Init_shapeslib() {
 	VALUE shapesModule = rb_define_module("ShapesLib");
-	rb_define_module_function(shapesModule,"open_shp",OpenSHP,2);
+	shapeClass = rb_define_class_under(shapesModule,"ShapeFile",rb_cObject);
+	rb_define_module_function(shapesModule,"open_shape",OpenSHP,2);
 }
 
 VALUE OpenSHP(VALUE klass,VALUE filename) {
+	VALUE rubyObject;
+	struct rbShapeFile *openedFile;
+	rubyObject = Data_Make_Struct(shapeClass,struct rbShapeFile,NULL,freeShapeFile,openedFile);
 	
+	char *cFileName = RSTRING(filename)->ptr;
+	openedFile->fileHandle = SHPOpen(cFileName,"rb+");
+	
+	rb_obj_call_init(rubyObject,NULL,NULL);
+	return rubyObject;
 }
 
