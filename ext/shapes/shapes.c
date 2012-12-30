4c10b2cd47327a066e7a6ce224ed768f5800c0d9
Omar Qazi <omar@predpol.com>
2012-12-30 00:39:52 -0800

    Added basic C IO functions

--- ext/shapes/shapes.c
+++ ext/shapes/shapes.c
@@ -4,12 +4,13 @@
 #include "shapefil.h"
 #include "shapes.h"
 
-VALUE shapeClass;
-	
 void Init_shapeslib() {
 	VALUE shapesModule = rb_define_module("ShapesLib");
 	shapeClass = rb_define_class_under(shapesModule,"ShapeFile",rb_cObject);
-	rb_define_module_function(shapesModule,"open_shape",OpenSHP,2);
+	rb_define_module_function(shapesModule,"open_shape",OpenSHP,1);
+	rb_define_module_function(shapesModule,"close_shape",CloseSHP,1);
+	rb_define_module_function(shapesModule,"create_shape",CreateSHP,2);
+	rb_define_module_function(shapesModule,"create_object",CreateObject,5);
 }
 
 VALUE OpenSHP(VALUE klass,VALUE filename) {
@@ -17,10 +18,110 @@ VALUE OpenSHP(VALUE klass,VALUE filename) {
 	struct rbShapeFile *openedFile;
 	rubyObject = Data_Make_Struct(shapeClass,struct rbShapeFile,NULL,freeShapeFile,openedFile);
 	
-	char *cFileName = RSTRING(filename)->ptr;
+	char *cFileName = StringValuePtr(filename);
 	openedFile->fileHandle = SHPOpen(cFileName,"rb+");
 	
-	rb_obj_call_init(rubyObject,NULL,NULL);
+	rb_obj_call_init(rubyObject,0,0);
 	return rubyObject;
 }
 
+VALUE CreateSHP(VALUE klass,VALUE filename,VALUE shapeType) {
+	VALUE rubyObject;
+	struct rbShapeFile *createdFile;
+	rubyObject = Data_Make_Struct(shapeClass,struct rbShapeFile,NULL,freeShapeFile,createdFile);
+	
+	const char *cFileName = StringValueCStr(filename);
+	
+	VALUE shapeTypeString = StringValue(shapeType);
+	const char *cShapeType = StringValueCStr(shapeTypeString);
+	
+	int shape = ShapeFromString(cShapeType);
+	
+	if (shape == -1) {
+		return Qfalse;
+	}
+	
+	createdFile->fileHandle = SHPCreate(cFileName,shape);
+	
+	rb_obj_call_init(rubyObject,0,0);
+	return rubyObject;
+}
+
+VALUE CreateObject(VALUE klass,VALUE handle,VALUE shapeType,VALUE x,VALUE y,VALUE z) {
+	struct rbShapeFile *shapeFile;
+	Data_Get_Struct(handle,struct rbShapeFile,shapeFile);
+	SHPHandle fileHandle = shapeFile->fileHandle;
+	
+	int xlen = RARRAY_LEN(x);
+	int ylen = RARRAY_LEN(y);
+	int zlen = RARRAY_LEN(z);
+	
+	
+	int xylen = (xlen < ylen) ? xlen : ylen; //Get the lesser of the two
+	int len = (zlen > 0 && zlen < xylen) ? zlen : xylen;
+	
+	const char *cShapeType = StringValueCStr(shapeType);
+	int shape = ShapeFromString(cShapeType);
+	
+	if (shape == -1) {
+		return Qfalse;
+	}
+	
+	double *xs, *ys, *zs;
+	xs = (double *)malloc(sizeof(double) * len);
+	ys = (double *)malloc(sizeof(double) * len);
+	zs = (double *)malloc(sizeof(double) * len);
+	
+	int i;
+	for (i = 0;i < len;i++) {
+		VALUE ax = rb_ary_pop(x);
+		VALUE ay = rb_ary_pop(y);
+		VALUE az = rb_ary_pop(z);
+		
+		xs[i] = NUM2DBL(ax);
+		ys[i] = NUM2DBL(ay);
+		
+		if (az != Qnil) {
+			zs[i] = az;
+		}
+	}
+	
+	if (zlen == 0) {
+		zs = NULL;
+	}
+	
+	SHPObject *newObject = SHPCreateSimpleObject(shape,len,xs,ys,zs);
+	int objNum = SHPWriteObject(fileHandle,-1,newObject);
+	SHPDestroyObject(newObject);
+	return INT2NUM(objNum);
+}
+
+VALUE CloseSHP(VALUE klass,VALUE handle) {
+	struct rbShapeFile *shapeFile;
+	Data_Get_Struct(handle,struct rbShapeFile,shapeFile);
+	SHPHandle fileHandle = shapeFile->fileHandle;
+	SHPClose(fileHandle);
+	return Qtrue;
+}
+
+void freeShapeFile(struct rbShapeFile *f) {
+	SHPClose(f->fileHandle);
+}
+
+int ShapeFromString(const char *cShapeType) {
+	int shape;
+	
+	if (strcmp(cShapeType,"point") == 0) {
+		shape = SHPT_POINT;
+	} else if (strcmp(cShapeType,"arc") == 0) {
+		shape = SHPT_ARC;	
+	} else if (strcmp(cShapeType,"polygon") == 0) {
+		shape = SHPT_POLYGON;
+	} else if (strcmp(cShapeType,"multipoint") == 0) {
+		shape = SHPT_MULTIPOINT;
+	} else {
+		shape = -1;		
+	}
+	
+	return shape;
+}
--- ext/shapes/shapes.h
+++ ext/shapes/shapes.h
@@ -1,3 +1,11 @@
 struct rbShapeFile {
 	SHPHandle fileHandle;	
 };
+
+VALUE shapeClass;
+VALUE OpenSHP(VALUE klass,VALUE filename);
+VALUE CreateSHP(VALUE klass,VALUE filename,VALUE shapeType);
+VALUE CloseSHP(VALUE klass,VALUE handle);
+VALUE CreateObject(VALUE klass,VALUE handle,VALUE shapeType,VALUE x,VALUE y,VALUE z);
+void freeShapeFile(struct rbShapeFile *f);
+int ShapeFromString(const char *cShapeType);
