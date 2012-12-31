24b7008d95b564035976918f64f10f3b58640a56
Omar Qazi <omar@predpol.com>
2012-12-30 18:45:14 -0800

    Implemented bridges for DBF functions

--- ext/shapes/shapes.c
+++ ext/shapes/shapes.c
@@ -5,12 +5,43 @@
 #include "shapes.h"
 
 void Init_shapeslib() {
-	VALUE shapesModule = rb_define_module("ShapesLib");
+	VALUE shapesModule = rb_define_module("ShapesLib"); //Create ShapesLib module
+	
+	//Define some classes to store our C data in
 	shapeClass = rb_define_class_under(shapesModule,"ShapeFile",rb_cObject);
+	dbfClass = rb_define_class_under(shapesModule,"DBF",rb_cObject);
+	
+	// Bridge for ShapeFile IO
 	rb_define_module_function(shapesModule,"open_shape",OpenSHP,1);
 	rb_define_module_function(shapesModule,"close_shape",CloseSHP,1);
 	rb_define_module_function(shapesModule,"create_shape",CreateSHP,2);
+	
+	// ShapeFile manipulation
 	rb_define_module_function(shapesModule,"create_object",CreateObject,6);
+	
+	// DBF IO
+	rb_define_module_function(shapesModule,"open_dbf",OpenDBF,1);
+	rb_define_module_function(shapesModule,"create_dbf",CreateDBF,1);
+	rb_define_module_function(shapesModule,"close_dbf",CloseDBF,1);
+	
+	// DBF manipulation
+	rb_define_module_function(shapesModule,"dbf_field_count",RDBFFieldCount,1);
+	rb_define_module_function(shapesModule,"dbf_record_count",RDBFRecordCount,1);
+	rb_define_module_function(shapesModule,"dbf_field_index",RDBFFieldIndex,2);
+	rb_define_module_function(shapesModule,"dbf_field_type",RDBFGetFieldType,2);
+	rb_define_module_function(shapesModule,"dbf_add_field",RDBFAddField,5);
+	
+	//Attribute read / write
+	rb_define_module_function(shapesModule,"dbf_read_int",RDBFReadInt,3);
+	rb_define_module_function(shapesModule,"dbf_read_double",RDBFReadDouble,3);
+	rb_define_module_function(shapesModule,"dbf_read_string",RDBFReadString,3);
+	rb_define_module_function(shapesModule,"dbf_attribute_null?",RDBFIsAttributeNull,3);
+	rb_define_module_function(shapesModule,"dbf_write_integer",RDBFWriteInt,4);
+	rb_define_module_function(shapesModule,"dbf_write_double",RDBFWriteDouble,4);
+	rb_define_module_function(shapesModule,"dbf_write_string",RDBFWriteString,4);
+	rb_define_module_function(shapesModule,"dbf_write_null",RDBFWriteNull,3);
+	rb_define_module_function(shapesModule,"dbf_record_deleted?",RDBFIsRecordDeleted,2);
+	rb_define_module_function(shapesModule,"dbf_delete_record",RDBFDeleteRecord,3);
 }
 
 VALUE OpenSHP(VALUE klass,VALUE filename) {
@@ -111,9 +142,320 @@ VALUE CloseSHP(VALUE klass,VALUE handle) {
 	return Qtrue;
 }
 
+//DBF IO
+VALUE OpenDBF(VALUE klass,VALUE filename) {
+	VALUE rubyObject;
+	struct rbDBF *openedFile;
+	rubyObject = Data_Make_Struct(dbfClass,struct rbDBF,NULL,freeDBF,openedFile);
+	
+	const char *cFileName = StringValueCStr(filename);
+	openedFile->fileHandle = DBFOpen(cFileName,"rb+");
+	openedFile->open = 1;
+	
+	rb_obj_call_init(rubyObject,0,0);
+	return rubyObject;
+}
+
+VALUE CreateDBF(VALUE klass,VALUE filename) {
+	VALUE rubyObject;
+	struct rbDBF *createdFile;
+	rubyObject = Data_Make_Struct(dbfClass,struct rbDBF,NULL,freeDBF,createdFile);
+	
+	const char *cFileName = StringValueCStr(filename);
+	
+	createdFile->fileHandle = DBFCreate(cFileName);
+	createdFile->open = 1;
+	
+	rb_obj_call_init(rubyObject,0,0);
+	return rubyObject;
+}
+
+VALUE CloseDBF(VALUE klass,VALUE handle) {
+	struct rbDBF *dbfFile;
+	Data_Get_Struct(handle,struct rbDBF,dbfFile);
+	DBFHandle fileHandle = dbfFile->fileHandle;
+	dbfFile->open = 0;
+	DBFClose(fileHandle);
+	return Qtrue;
+}
+
+//DBF Manipulation
+
+VALUE RDBFFieldCount(VALUE klass,VALUE handle) {
+	struct rbDBF *dbf;
+	Data_Get_Struct(handle,struct rbDBF,dbf);
+	DBFHandle d = dbf->fileHandle;
+	
+	int fieldCount = DBFGetFieldCount(d);
+	VALUE rv = INT2NUM(fieldCount);
+	
+	return rv;
+}
+
+VALUE RDBFRecordCount(VALUE klass,VALUE handle) {
+	struct rbDBF *dbf;
+	Data_Get_Struct(handle,struct rbDBF,dbf);
+	DBFHandle d = dbf->fileHandle;
+	
+	int recordCount = DBFGetRecordCount(d);
+	VALUE rv = INT2NUM(recordCount);
+	
+	return rv;
+}
+
+VALUE RDBFFieldIndex(VALUE klass,VALUE handle,VALUE fieldName) {
+	struct rbDBF *dbf;
+	Data_Get_Struct(handle,struct rbDBF,dbf);
+	DBFHandle d = dbf->fileHandle;
+	
+	const char *cFieldName = StringValueCStr(fieldName);
+	
+	int index = DBFGetFieldIndex(d,cFieldName);
+	VALUE rv = INT2NUM(index);
+	
+	return rv;
+}
+
+VALUE RDBFGetFieldType(VALUE klass,VALUE handle,VALUE fieldIndex) {
+	struct rbDBF *dbf;
+	Data_Get_Struct(handle,struct rbDBF,dbf);
+	DBFHandle d = dbf->fileHandle;
+	
+	int cFieldIndex = NUM2INT(fieldIndex);
+	
+	DBFFieldType fieldType = DBFGetFieldInfo(d,cFieldIndex,NULL,NULL,NULL);
+	
+	VALUE fieldSymbol;
+	
+	switch (fieldType) {
+	case FTString:
+		fieldSymbol = ID2SYM(rb_intern("string"));
+		break;
+	case FTInteger:
+		fieldSymbol = ID2SYM(rb_intern("integer"));
+		break;
+	case FTDouble:
+		fieldSymbol = ID2SYM(rb_intern("double"));
+		break;
+	case FTLogical:
+		fieldSymbol = ID2SYM(rb_intern("logical"));
+		break;
+	case FTInvalid:
+		fieldSymbol = ID2SYM(rb_intern("invalid"));
+		break;
+	}
+	
+	return fieldSymbol;
+}
+
+VALUE RDBFAddField(VALUE klass,VALUE handle,VALUE fieldName,VALUE fieldType,VALUE width,VALUE decimals) {
+	struct rbDBF *dbf;
+	Data_Get_Struct(handle,struct rbDBF,dbf);
+	DBFHandle d = dbf->fileHandle;
+	
+	const char *cFieldName = StringValueCStr(fieldName);
+	const char *cFieldType = StringValueCStr(fieldType);
+	
+	int cWidth    = NUM2INT(width);
+	int cDecimals = NUM2INT(decimals);
+	
+	DBFFieldType ft = FieldTypeFromString(cFieldType);
+	printf("so far so nig\n");
+	int fieldNumber = DBFAddField(d,cFieldName,ft,cWidth,cDecimals);
+	printf("made it through that\n");
+	VALUE rv = INT2NUM(fieldNumber);
+	
+	return rv;
+}
+
+VALUE RDBFReadInt(VALUE klass,VALUE handle,VALUE row,VALUE field) {
+	struct rbDBF *dbf;
+	Data_Get_Struct(handle,struct rbDBF,dbf);
+	DBFHandle d = dbf->fileHandle;
+	
+	int cRow = NUM2INT(row);
+	int cField = NUM2INT(field);
+	
+	int r = DBFReadIntegerAttribute(d,cRow,cField);
+	VALUE rv = INT2NUM(r);
+	
+	return rv;
+}
+
+VALUE RDBFReadDouble(VALUE klass,VALUE handle,VALUE row,VALUE field) {
+	struct rbDBF *dbf;
+	Data_Get_Struct(handle,struct rbDBF,dbf);
+	DBFHandle d = dbf->fileHandle;
+	
+	int cRow = NUM2INT(row);
+	int cField = NUM2INT(field);
+	
+	double r = DBFReadDoubleAttribute(d,cRow,cField);
+	VALUE rv = DOUBLE2NUM(r);
+	
+	return rv;
+}
+
+VALUE RDBFReadString(VALUE klass,VALUE handle,VALUE row,VALUE field) {
+	struct rbDBF *dbf;
+	Data_Get_Struct(handle,struct rbDBF,dbf);
+	DBFHandle d = dbf->fileHandle;
+	
+	int cRow = NUM2INT(row);
+	int cField = NUM2INT(field);
+	
+	const char *r = DBFReadStringAttribute(d,cRow,cField);
+	VALUE rv = rb_str_new2(r);
+	
+	return rv;
+}
+
+VALUE RDBFIsAttributeNull(VALUE klass,VALUE handle,VALUE row,VALUE field) {
+	struct rbDBF *dbf;
+	Data_Get_Struct(handle,struct rbDBF,dbf);
+	DBFHandle d = dbf->fileHandle;
+	
+	int cRow = NUM2INT(row);
+	int cField = NUM2INT(field);
+	
+	VALUE rv = Qfalse;
+	
+	if (DBFIsAttributeNull(d,cRow,cField) == TRUE) {
+		rv = Qtrue;
+	}
+	
+	return rv;
+}
+
+VALUE RDBFWriteInt(VALUE klass,VALUE handle,VALUE row,VALUE field,VALUE val) {
+	struct rbDBF *dbf;
+	Data_Get_Struct(handle,struct rbDBF,dbf);
+	DBFHandle d = dbf->fileHandle;
+	
+	int cRow = NUM2INT(row);
+	int cField = NUM2INT(field);
+	int cVal = NUM2INT(val);
+	
+	VALUE rv;
+	
+	if (DBFWriteIntegerAttribute(d,cRow,cField,cVal) == TRUE) {
+		rv = Qtrue;
+	} else {
+		rv = Qfalse;		
+	}
+	
+	return rv;
+}
+
+VALUE RDBFWriteDouble(VALUE klass,VALUE handle,VALUE row,VALUE field,VALUE val) {
+	struct rbDBF *dbf;
+	Data_Get_Struct(handle,struct rbDBF,dbf);
+	DBFHandle d = dbf->fileHandle;
+	
+	int cRow = NUM2INT(row);
+	int cField = NUM2INT(field);
+	double cVal = NUM2DBL(val);
+	
+	VALUE rv;
+	
+	if (DBFWriteDoubleAttribute(d,cRow,cField,cVal) == TRUE) {
+		rv = Qtrue;
+	} else {
+		rv = Qfalse;		
+	}
+	
+	return rv;
+}
+
+VALUE RDBFWriteString(VALUE klass,VALUE handle,VALUE row,VALUE field,VALUE val) {
+	struct rbDBF *dbf;
+	Data_Get_Struct(handle,struct rbDBF,dbf);
+	DBFHandle d = dbf->fileHandle;
+	
+	int cRow = NUM2INT(row);
+	int cField = NUM2INT(field);
+	const char *cVal = StringValueCStr(val);
+	
+	VALUE rv;
+	
+	if (DBFWriteStringAttribute(d,cRow,cField,cVal) == TRUE) {
+		rv = Qtrue;
+	} else {
+		rv = Qfalse;		
+	}
+	
+	return rv;
+}
+
+VALUE RDBFWriteNull(VALUE klass,VALUE handle,VALUE row,VALUE field) {
+	struct rbDBF *dbf;
+	Data_Get_Struct(handle,struct rbDBF,dbf);
+	DBFHandle d = dbf->fileHandle;
+	
+	int cRow = NUM2INT(row);
+	int cField = NUM2INT(field);
+	
+	VALUE rv;
+	
+	if (DBFWriteNULLAttribute(d,cRow,cField) == TRUE) {
+		rv = Qtrue;
+	} else {
+		rv = Qfalse;		
+	}
+	
+	return rv;
+}
+
+VALUE RDBFIsRecordDeleted(VALUE klass,VALUE handle,VALUE row) {
+	struct rbDBF *dbf;
+	Data_Get_Struct(handle,struct rbDBF,dbf);
+	DBFHandle d = dbf->fileHandle;
+	
+	int cRow = NUM2INT(row);
+	
+	VALUE rv;
+	
+	if (DBFIsRecordDeleted(d,cRow) == TRUE) {
+		rv = Qtrue;
+	} else {
+		rv = Qfalse;
+	}
+	
+	return rv;
+}
+
+VALUE RDBFDeleteRecord(VALUE klass,VALUE handle,VALUE row,VALUE val) {
+	struct rbDBF *dbf;
+	Data_Get_Struct(handle,struct rbDBF,dbf);
+	DBFHandle d = dbf->fileHandle;
+	
+	int cRow = NUM2INT(row);
+	int cVal  = NUM2INT(val);
+	
+	int deleted = DBFMarkRecordDeleted(d,cRow,cVal);
+	
+	VALUE rv;
+	
+	if (deleted == TRUE) {
+		rv = Qtrue;	
+	} else {
+		rv = Qfalse;		
+	}
+	
+	return rv;
+}
+
 void freeShapeFile(struct rbShapeFile *f) {
 	if (f->open) {
+		f->open = 0;
 		SHPClose(f->fileHandle);	
+	}
+}
+
+void freeDBF(struct rbDBF *f) {
+	if (f->open) {
+		f->open = 0;
+		DBFClose(f->fileHandle);
 	}
 }
 
@@ -133,4 +475,22 @@ int ShapeFromString(const char *cShapeType) {
 	}
 	
 	return shape;
+}
+
+DBFFieldType FieldTypeFromString(const char *cFieldType) {
+	DBFFieldType fieldType;
+	
+	if (strcmp(cFieldType,"string") == 0) {
+		fieldType = FTString;
+	} else if (strcmp(cFieldType,"integer") == 0) {
+		fieldType = FTInteger;
+	} else if (strcmp(cFieldType,"double") == 0) {
+		fieldType = FTDouble;		
+	} else if (strcmp(cFieldType,"logical") == 0) {
+		fieldType = FTLogical;
+	} else {
+		fieldType = FTInvalid;
+	}
+	
+	return fieldType;
 }
--- ext/shapes/shapes.h
+++ ext/shapes/shapes.h
@@ -1,12 +1,53 @@
+#define TRUE		1
+#define	FALSE		0
+
 struct rbShapeFile {
 	int open;
 	SHPHandle fileHandle;
 };
 
+struct rbDBF {
+	int open;
+	DBFHandle fileHandle;
+};
+
 VALUE shapeClass;
+VALUE dbfClass;
+
+// ShapeFile IO
 VALUE OpenSHP(VALUE klass,VALUE filename);
 VALUE CreateSHP(VALUE klass,VALUE filename,VALUE shapeType);
 VALUE CloseSHP(VALUE klass,VALUE handle);
+
+//ShapeFile manipulation
 VALUE CreateObject(VALUE klass,VALUE handle,VALUE shapeType,VALUE numVertices,VALUE x,VALUE y,VALUE z);
+
+//DBF IO
+VALUE OpenDBF(VALUE klass,VALUE filename);
+VALUE CreateDBF(VALUE klass,VALUE filename);
+VALUE CloseDBF(VALUE klass,VALUE handle);
+
+//DBF Manipulation
+
+VALUE RDBFFieldCount(VALUE klass,VALUE handle);
+VALUE RDBFRecordCount(VALUE klass,VALUE handle);
+VALUE RDBFFieldIndex(VALUE klass,VALUE handle,VALUE fieldName);
+VALUE RDBFGetFieldType(VALUE klass,VALUE handle,VALUE fieldIndex);
+VALUE RDBFAddField(VALUE klass,VALUE handle,VALUE fieldName,VALUE fieldType,VALUE width,VALUE decimals);
+
+// DBF read / write
+VALUE RDBFReadInt(VALUE klass,VALUE handle,VALUE row,VALUE field);
+VALUE RDBFReadDouble(VALUE klass,VALUE handle,VALUE row,VALUE field);
+VALUE RDBFReadString(VALUE klass,VALUE handle,VALUE row,VALUE field);
+VALUE RDBFIsAttributeNull(VALUE klass,VALUE handle,VALUE row,VALUE field);
+VALUE RDBFWriteInt(VALUE klass,VALUE handle,VALUE row,VALUE field,VALUE val);
+VALUE RDBFWriteDouble(VALUE klass,VALUE handle,VALUE row,VALUE field,VALUE val);
+VALUE RDBFWriteString(VALUE klass,VALUE handle,VALUE row,VALUE field,VALUE val);
+VALUE RDBFWriteNull(VALUE klass,VALUE handle,VALUE row,VALUE field);
+VALUE RDBFIsRecordDeleted(VALUE klass,VALUE handle,VALUE row);
+VALUE RDBFDeleteRecord(VALUE klass,VALUE handle,VALUE row,VALUE val);
+	
 void freeShapeFile(struct rbShapeFile *f);
+void freeDBF(struct rbDBF *f);
 int ShapeFromString(const char *cShapeType);
+DBFFieldType FieldTypeFromString(const char *cFieldType);
